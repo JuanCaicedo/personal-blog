d913021e7fbc7cfa4e3e4ec3cbca03c686ea69ac
Juan Caicedo <retiredcanadianpoet@gmail.com>
2018-02-18 12:11:14 +0800

    two cards

--- components/Card.js
+++ components/Card.js
@@ -1,3 +1,5 @@
-const Card = ({ children }) => <div>{children}</div>
+const Card = ({ children, header }) => (
+  <div className="bg-yellow pa3 mb3">{children}</div>
+)
 
 export default Card
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,25 @@
 import Layout from '../layout/Main'
 import Card from '../components/Card'
+import AboutMe from '../content/about-me.mdx'
+import Talks from '../content/conference-talks.mdx'
 
-export default () => (
+const Page = () => (
   <Layout title="Juan Caicedo">
+    <style jsx>{`
+      :global(:first-child) {
+        margin-top: 0;
+      }
+      :global(:last-child) {
+        margin-bottom: 0;
+      }
+    `}</style>
     <Card>
-      <h3>About me</h3>
-      <div>
-        My name is Juan Caicedo, I like coding, adventuring, and ice cream
-        sandwiches.
-      </div>
+      <AboutMe />
+    </Card>
+    <Card>
+      <Talks />
     </Card>
   </Layout>
 )
+
+export default Page
