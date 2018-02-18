aec26d616ebcbd8674d887d397f7cc621cbd4c7e
Juan Caicedo <retiredcanadianpoet@gmail.com>
2018-02-18 13:50:51 +0800

    fork on github

--- layout/Main.js
+++ layout/Main.js
@@ -11,6 +11,19 @@ export default ({ children, title = 'This is the default title' }) => (
       <Nav />
     </header>
 
+    <style jsx>{`
+      a {
+        top: 50px;
+        left: -70px;
+      }
+    `}</style>
+    <a
+      href="https://github.com/juancaicedo/personal-blog"
+      className="near-white bg-red pv2 ph5 rotate-315 dib absolute"
+    >
+      Fork me on GitHub
+    </a>
+
     <main className="mw-100 mw7-ns center">{children}</main>
 
     <Footer>Footer</Footer>
