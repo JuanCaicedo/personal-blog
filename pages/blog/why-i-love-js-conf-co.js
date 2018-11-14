5161454f03542eddd536b2e0c7c25b2a520d77bb
Juan Caicedo <retiredcanadianpoet@gmail.com>
2018-11-14 10:08:48 -0500

    jsconf colombia (#7)
    * jsconf colombia
    
    * update jsconf colombia
    
    * Emoji and updates

---
+++ pages/blog/why-i-love-js-conf-co.js
@@ -0,0 +1,14 @@
+import Layout from '../../layout/Main';
+import Content from '../../content/js-conf-colombia.mdx';
+import Card from '../../components/Card';
+import 'prismjs/themes/prism.css';
+
+const Post = () => (
+  <Layout title="Why I love JSConf 🇨🇴">
+    <Card>
+      <Content />
+    </Card>
+  </Layout>
+);
+
+export default Post;
