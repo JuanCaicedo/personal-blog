ba7f982447e9b28846534b3dc1c0de13e66b5b33
Juan Caicedo <retiredcanadianpoet@gmail.com>
2018-12-17 16:46:26 -0500

    extract blog styles

--- layout/Main.js
+++ layout/Main.js
@@ -1,43 +1,10 @@
 import Head from 'next/head'
 import Nav from './Nav'
 import Footer from './Footer'
+import './blog.css'
 
 export default ({ children, title = 'This is the default title' }) => (
   <div className="helvetica">
-    <style jsx>{`
-      :global(a) {
-        font-weight: 600;
-        color: #ff4136;
-        text-decoration: none;
-      }
-
-      :global(a:hover) {
-        text-decoration: underline;
-      }
-
-      :global(html *) {
-        font-size: 2rem;
-      }
-
-      :global(.juan-blog p) {
-        padding-bottom: 3rem;
-        font-size: 3rem;
-      }
-
-      :global(.juan-blog a) {
-        font-size: 3rem;
-      }
-
-      :global(.juan-blog h1) {
-        padding-bottom: 5rem;
-        font-size: 5rem;
-      }
-
-      :global(.juan-blog h2) {
-        padding-bottom: 4rem;
-        font-size: 4rem;
-      }
-    `}</style>
     <Head>
       <title>{title}</title>
     </Head>
--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default class MyDocument extends Document {
   render() {
     return (
       <html className="bg-blue">
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
         <Head>
           <link
             href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css"
