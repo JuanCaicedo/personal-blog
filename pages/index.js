9e19dead509aabf1511617948205ce49067dc0e1
Juan Caicedo <retiredcanadianpoet@gmail.com>
2019-01-07 09:58:42 -0500

    smaller cards

--- pages/index.js
+++ pages/index.js
@@ -4,27 +4,32 @@ import AboutMe from '../content/about-me.mdx'
 import Talks from '../content/conference-talks.mdx'
 import Blog from '../content/blog.mdx'
 
+const Cards = [
+  <div>
+    Hi! I'm Juan Caicedo, a Colombian/Canadian/American full-stack JS developer.
+  </div>,
+  <Blog />,
+  <Talks />
+]
 const Page = () => (
   <Layout title="Juan Caicedo">
+    <style jsx>{`
+      .content {
+        width: 450px;
+      }
+    `}</style>
     <img
       src="/static/juan.jpg"
-      className="center db mb4 overflow-hidden br2 w-2/3 block m-auto mb-6"
+      className="center db mb4 overflow-hidden br2 block m-auto mb-6 w-64"
     />
 
-    <Card>
-      <div>
-        Hi! I'm Juan Caicedo, a Colombian/Canadian/American full stack JS
-        developer.
-      </div>
-    </Card>
-
-    <Card>
-      <Blog />
-    </Card>
-
-    <Card>
-      <Talks />
-    </Card>
+    <div className="content m-auto">
+      {Cards.map((content, i) => (
+        <Card className="ser center" key={`card-${i}`}>
+          {content}
+        </Card>
+      ))}
+    </div>
   </Layout>
 )
 
