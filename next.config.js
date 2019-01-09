3e4ccb22f721b3e6e1a0e54b092790b4ec59face
Juan Caicedo <retiredcanadianpoet@gmail.com>
2019-01-09 10:02:02 -0500

    scope next middleware

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
-const withMdx = require('@zeit/next-mdx')()
-const withCSS = require('@zeit/next-css')
-module.exports = withMdx(
-  withCSS({
-    target: 'serverless'
-  })
-)
+module.exports = () => {
+  const withMdx = require('@zeit/next-mdx')()
+  const withCSS = require('@zeit/next-css')
+
+  return withMdx(
+    withCSS({
+      target: 'serverless'
+    })
+  )
+}
