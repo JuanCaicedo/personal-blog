bf8f03398bfb185e8ff95c2125b003a8597ba509
Juan Caicedo <retiredcanadianpoet@gmail.com>
2019-01-09 10:53:49 -0500

    update to now 2 (#11)
    Zeit has released a new version which is supposed to be much better and faster.
    Let's give it a try 😄

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
-const withMdx = require('@zeit/next-mdx')()
-const withCSS = require('@zeit/next-css')
-module.exports = withMdx(withCSS({}))
+module.exports = () => {
+  const withMdx = require('@zeit/next-mdx')()
+  const withCSS = require('@zeit/next-css')
+
+  return withMdx(
+    withCSS({
+      target: 'serverless'
+    })
+  )
+}
