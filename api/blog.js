0690dacdd1c5a859e53646d17899b7c11e597676
Juan Caicedo <retiredcanadianpoet@gmail.com>
2019-01-11 15:02:56 -0500

    render first page

--- api/blog.js
+++ api/blog.js
@@ -7,7 +7,7 @@ const util = require('util')
 const fm = require('front-matter')
 
 const readDirAsync = util.promisify(fs.readdir)
-const readFile = util.promisify(fs.readFile)
+const readFileAsync = util.promisify(fs.readFile)
 
 const postsPath = path.resolve(__dirname, '../posts')
 
@@ -17,13 +17,22 @@ const listAllPosts = async () => {
   const fileContents = await Promise.all(
     fileNames
       .filter(name => /mdx$/.test(name))
-      .map(name => readFile(`${postsPath}/${name}`, 'utf8'))
+      .map(name => readFileAsync(`${postsPath}/${name}`, 'utf8'))
   )
   const result = fileContents.map(file => fm(file)).map(file => {
     return file.attributes
   })
 
   return result
+}
+
+const findPostBySlug = async (req, res) => {
+  const content = await readFileAsync(
+    `${postsPath}/${req.params.slug}.mdx`,
+    'utf8'
+  )
+  const { attributes, body } = fm(content)
+  return { ...attributes, content: body }
 }
 
 const list = async (req, res) => {
@@ -36,4 +45,8 @@ const list = async (req, res) => {
 }
 const notfound = async (req, res) => send(res, 404, 'Not found route')
 
-module.exports = router(get('/list', cors(list)), get('/*', notfound))
+module.exports = router(
+  get('/list', cors(list)),
+  get('/:slug', cors(findPostBySlug)),
+  get('/*', notfound)
+)
--- pages/blog/copy-last-command.js
+++ pages/blog/copy-last-command.js
@@ -1,14 +1,36 @@
+import ReactMarkdown from 'react-markdown'
+import axios from 'axios'
+
 import Layout from '../../layout/Main'
-import Content from '../../content/copy-last-command.mdx'
 import Card from '../../components/Card'
 import 'prismjs/themes/prism.css'
 
-const Post = () => (
+const Post = ({ content }) => (
   <Layout title="How to copy last command in bash">
     <Card className="juan-blog">
-      <Content />
+      <ReactMarkdown source={content} />,
     </Card>
   </Layout>
 )
+
+const getPost = async (slug, req) => {
+  const baseUrl =
+    process.env.NODE_ENV === 'production'
+      ? `${process.env.API_URL}/api/blog/`
+      : `${process.env.API_URL}/`
+  const response = await axios(`${baseUrl}/${slug}`)
+  const json = response.data
+  return json
+}
+
+Post.getInitialProps = async ({ req }) => {
+  try {
+    const { content } = await getPost('copy-last-command', req)
+    return { content }
+  } catch (e) {
+    console.error('e', e)
+    return {}
+  }
+}
 
 export default Post
