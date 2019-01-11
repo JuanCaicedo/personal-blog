3b631659253617666ff5242b76f15515b93a3056
Juan Caicedo <retiredcanadianpoet@gmail.com>
2019-01-11 13:55:36 -0500

    support reading front matter

--- api/blog.js
+++ api/blog.js
@@ -3,14 +3,29 @@ const { router, get } = require('microrouter')
 const fs = require('fs')
 const path = require('path')
 const util = require('util')
+const fm = require('front-matter')
 
 const readDirAsync = util.promisify(fs.readdir)
-const listAllPosts = () => readDirAsync(path.resolve(__dirname, '../content'))
+const readFile = util.promisify(fs.readFile)
+
+const contentPath = path.resolve(__dirname, '../content')
+
+const listAllPosts = async () => {
+  const fileNames = await readDirAsync(contentPath)
+  const fileContents = await Promise.all(
+    fileNames.map(name => readFile(`${contentPath}/${name}`, 'utf8'))
+  )
+  const result = fileContents.map(file => fm(file)).map(file => {
+    console.log('file', file)
+    return file.attributes
+  })
+
+  return result
+}
 
 const list = async (req, res) => {
   try {
     const all = await listAllPosts()
-    console.log('all', all)
     send(res, 200, all)
   } catch (e) {
     console.error('Error in reading posts', e)
