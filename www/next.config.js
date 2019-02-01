92ca8072ca965720ade87455d9cfa14c4be8a095
Juan Caicedo <retiredcanadianpoet@gmail.com>
2019-02-01 15:46:09 -0500

    remove mdx

--- www/next.config.js
+++ www/next.config.js
@@ -1,10 +1,7 @@
 module.exports = () => {
-  const withMdx = require('@zeit/next-mdx')()
   const withCSS = require('@zeit/next-css')
 
-  return withMdx(
-    withCSS({
-      target: 'serverless'
-    })
-  )
+  return withCSS({
+    target: 'serverless'
+  })
 }
