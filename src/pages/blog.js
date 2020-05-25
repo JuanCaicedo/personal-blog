598c72d5e62aa6dbbd3c5eb4a9caace7fe7fc2d1
Juan Caicedo <retiredcanadianpoet@gmail.com>
2020-05-25 13:04:47 -0700

    sort blog index by date

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,23 +1,30 @@
 import React from 'react'
 import { Link, graphql } from 'gatsby'
 import * as R from 'ramda'
+import { format } from 'date-fns'
 
 import Layout from '../layout/Main'
 import Card from '../components/Card'
 
+function Post({ slug, title, date }) {
+  const formatted = format(new Date(date), 'MMMM do yyyy')
+  return (
+    <Card className="w-full">
+      <div>
+        <Link to={`/blog/${slug}`}>
+          <h3>{title}</h3>
+        </Link>
+        <div>Posted on {formatted}</div>
+      </div>
+    </Card>
+  )
+}
 function Index({ posts = [] }) {
   return (
     <Layout title="Blog">
       <h1 className="mb-8 text-center">Previous posts</h1>
-      {posts.map(({ title, date, slug }, i) => (
-        <Card key={i} className="w-full">
-          <div>
-            <Link to={`/blog/${slug}`}>
-              <h3>{title}</h3>
-            </Link>
-            <div>{date}</div>
-          </div>
-        </Card>
+      {posts.map((post, i) => (
+        <Post key={i} {...post} />
       ))}
     </Layout>
   )
@@ -30,7 +37,7 @@ export default function Page({ data }) {
 
 export const pageQuery = graphql`
   query {
-    allMdx {
+    allMdx(sort: { order: DESC, fields: frontmatter___date }) {
       nodes {
         frontmatter {
           title
